709e2a30e1a4703ee46c38b8e0eefeb92cb61fd6
Abdallah <Abdallah97158@gmail.com>
2022-12-18 11:51:50 +0200

    js lab 1

---
+++ labs/js/lab1/index.js
@@ -0,0 +1,33 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function askUser() {
+  var num1 = parseInt(prompt("please enter two number \n number 1:"));
+  var num2 = parseInt(prompt("please enter second number"));
+
+  document.write(sum(num1, num2));
+}
+// 3
+function evalute() {
+  var exp = prompt("enter expression here ");
+  var result = eval(exp);
+
+  alert("You expression :" + exp + " and evalute to : " + result);
+}
+
+// 4 contact info
+
+// bounce
+// Try this code using strict mode and without strict mode, what’s the difference? And why?
+// function foo() {
+// 	var x;
+// 	x = 5;
+// 	y = 6;	return x + y;
+// }
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
+console.log("dlkcjsdlkfjskldfjs".charAt(-10));
